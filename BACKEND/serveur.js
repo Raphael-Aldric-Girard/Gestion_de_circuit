a3ef81f9ef0ba248f622e8cb3a655eedca58d516
Devaux <pierre-louis.devaux@outlook.com>
2025-12-08 14:53:35 +0100

    modifications des droits d'accès

--- BACKEND/serveur.js
+++ BACKEND/serveur.js
@@ -6,7 +6,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors({
-    origin: '172.16.194.254', // En production, spécifiez l'origine exacte
+    origin: 'http://172.16.194.254', // En production, spécifiez l'origine exacte
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
